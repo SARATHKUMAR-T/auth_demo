4782f498eab145789e6d58b6524d9c088db34ffb
sarathkumar.thangavel@aximsoft.com-at-327859520898 <sarathkumar.thangavel@aximsoft.com-at-327859520898>
2024-10-18 16:46:50 +0530

    prod build changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
-import axios from "axios";
-import { clsx, type ClassValue } from "clsx"
-import { json } from "stream/consumers";
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
